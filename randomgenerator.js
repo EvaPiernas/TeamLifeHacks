d07086fc2ca38eb94c5ed37b36a65b9084d5a0f0
EvaPiernas <gwc@Girls-Who-Code-2.local>
2016-08-03 10:31:56 -0400

    change

--- randomgenerator.js
+++ randomgenerator.js
@@ -1,3 +1,5 @@
+var randomValue = myArray[Math.floor(Math.random() * myArray.length)];
 
-
+var myShows = ['Bones', 'Psych', 'Big Bang Theory', 'Mad Men', 
+'Breaking Bad', 'Modern Family', 'Game of Thrones', 'Dexter'];
 
