607d01d7010650c631ca9d5d3d2c2fd3fb655428
EvaPiernas <gwc@Girls-Who-Code-2.local>
2016-08-03 13:30:52 -0400

    c

--- randomgenerator1.js
+++ randomgenerator1.js
@@ -1,8 +1,7 @@
 // random genrator button function
 
-
 function myFunction() {
-list = ['Bones', 'Psych', 'Big Bang Theory', 'Mad Men'];
-r = Math.floor(Math.random() * 4);
-return list[r];
-}
+list = ['Bones', 'Psych', 'Big Bang Theory', 'Mad Men']
+r = Math.floor(Math.random() * 4)
+return list[r]
+};
