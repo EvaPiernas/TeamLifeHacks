a0f32ff341549efac5bcb79dccb4b2d1f3b28a3b
ksainclair2 <kerryannsainclair@gmail.com>
2016-08-03 11:41:39 -0400

    h

--- randomgenerator1.js
+++ randomgenerator1.js
@@ -1,5 +1,19 @@
-// random genrator button function
-list = ['Bones', 'Psych', 'Big Bang Theory', 'Mad Men']
+// // random genrator button function
+// list = ['Bones', 'Psych', 'Big Bang Theory', 'Mad Men']
 
-r = Math.floor((Math.random() * 4) 
-list[r]
+// r = Math.floor((Math.random() * 4) 
+// list[r]
+
+
+
+
+<button onclick="myFunction()">Try it</button>
+
+<p id="demo"></p>
+
+<script>
+function myFunction() {
+    var x = Math.floor((Math.random() * 10) + 1);
+    document.getElementById("demo").innerHTML = x;
+}
+</script>
