78f88c3f0f90c98436f30ac7445e1368b3b8de52
EvaPiernas <gwc@Girls-Who-Code-2.local>
2016-08-03 13:57:45 -0400

    c

--- ourJS.js
+++ ourJS.js
@@ -11,5 +11,6 @@ function openRequestedPopup() {
 function myFunction() {
 list = ['Bones', 'Psych', 'Big Bang Theory', 'Mad Men'];
 r = Math.floor(Math.random() * 4);
-return list[r];
+// return list[r];
+document.write(list[r]);
 }
