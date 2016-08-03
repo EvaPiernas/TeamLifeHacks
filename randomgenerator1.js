dff66d07ff03dd96643696431a4e4d83ba2b2d45
EvaPiernas <gwc@Girls-Who-Code-2.local>
2016-08-03 11:28:16 -0400

    change

--- randomgenerator1.js
+++ randomgenerator1.js
@@ -1,9 +1,5 @@
 // random genrator button function
-var myShows = ['Bones', 'Psych', 'Big Bang Theory', 'Mad Men', 
-'Breaking Bad', 'Modern Family', 'Game of Thrones', 'Dexter']; 
+list = ['Bones', 'Psych', 'Big Bang Theory', 'Mad Men']
 
-var show = myShows[Math.floor(Math.random() * myShows.length)];
-
-function show() { 
-myShows[Math.floor(Math.random() * myShows.length)]; 
-}
+r = Math.floor(Math.random() * 4) 
+list[r]
