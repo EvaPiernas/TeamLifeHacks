389704e868301f94b98d5d7297857bd88b9e9134
EvaPiernas <gwc@Girls-Who-Code-2.local>
2016-08-03 10:27:21 -0400

    change

--- randomgenerator.js
+++ randomgenerator.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-var myArray = ['schoolhacks.html', 'techhacks.html','healthhacks.html','clotheshacks.html',]
-var rand = myArray[Math.floor() * myArray.length]
->>>>>>> origin/master
-=======
->>>>>>> origin/master
+
 
 
