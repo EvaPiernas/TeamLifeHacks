4c1d1a9445140f54c8316ea66d14ebde97bde1fb
EvaPiernas <gwc@Girls-Who-Code-2.local>
2016-08-03 11:11:44 -0400

    new

--- ourJS.js
+++ ourJS.js
@@ -1,13 +1,18 @@
+// pop up window
 var windowObjectReference;
-
 function openRequestedPopup() {
   windowObjectReference = window.open(
     "lifehackshome.html",
     "DescriptiveWindowName",
     "resizable,scrollbars,status"
+    )};
 
-// random genrator button function
-var show = myShows[Math.floor(Math.random() * myShows.length)];
-
+// random generator button function
 var myShows = ['Bones', 'Psych', 'Big Bang Theory', 'Mad Men', 
-'Breaking Bad', 'Modern Family', 'Game of Thrones', 'Dexter'];  
+'Breaking Bad', 'Modern Family', 'Game of Thrones', 'Dexter']; 
+
+function show() { 
+myShows[Math.floor(Math.random() * myShows.length)]; 
+}
+
+	
---
+++ randomgenerator1.js
@@ -0,0 +1,9 @@
+// random genrator button function
+var myShows = ['Bones', 'Psych', 'Big Bang Theory', 'Mad Men', 
+'Breaking Bad', 'Modern Family', 'Game of Thrones', 'Dexter']; 
+
+var show = myShows[Math.floor(Math.random() * myShows.length)];
+
+function show() { 
+myShows[Math.floor(Math.random() * myShows.length)]; 
+}
