86749d437b7a4ffac9e71181b88b402c317d7674
EvaPiernas <gwc@Girls-Who-Code-2.local>
2016-08-04 10:45:54 -0400

    change

--- ourJS.js
+++ ourJS.js
@@ -18,9 +18,3 @@
 // return list[r];
 // }
 
-function myFunction() {
-    list = [lh, lh1, lh2, lh3, lh4, lh5];
-    // this is so that we can keep the randy button 
-	r = Math.floor(Math.random() * 6);
-    document.getElementById("randomValue").innerHTML = list[r];
-    }
