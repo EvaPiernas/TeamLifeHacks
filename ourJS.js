07d45fd36096579efa9f7852cb90d28ef6c04097
EvaPiernas <gwc@Girls-Who-Code-2.local>
2016-08-04 09:41:29 -0400

    c

--- ourJS.js
+++ ourJS.js
@@ -11,9 +11,16 @@
   
 // random genrator button function
 
-function myFunction() {
-list = ['Bones', 'Psych', 'Big Bang Theory', 'Mad Men'];
-r = Math.floor(Math.random() * 4);
+// function myFunction() {
+// list = ['Bones', 'Psych', 'Big Bang Theory', 'Mad Men'];
+// r = Math.floor(Math.random() * 4);
+// // return list[r];
 // return list[r];
-return list[r];
-}
+// }
+
+function myFunction() {
+    list = [var lh, var lh1, var lh2, var lh3, var lh4, var lh5];
+    // this is so that we can keep the randy button 
+	r = Math.floor(Math.random() * 6);
+    document.getElementById("randomValue").innerHTML = list[r];
+    }
